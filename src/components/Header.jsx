5d517b5b45ebced43287d2463a81531f39f21548
Marharyta Levtsun <marharyta.levtsun@wundermanthompson.com>
2022-02-18 11:31:41 +0100

    Initial template

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header className="header">
+        <img src="https://www.clipartmax.com/png/middle/39-395435_3d-art-logo-design-free-logos-online-logo-design-free-png.png" alt=""/>
+    </header>
+    )
+}
+
+export default Header;
